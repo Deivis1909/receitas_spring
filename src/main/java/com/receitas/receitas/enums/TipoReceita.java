4e98095353ee60998fb6663bcdb040993718671b
Deivis1909 <113397116+Deivis1909@users.noreply.github.com>
2023-07-29 15:06:53 -0300

    atualiza imports

--- src/main/java/com/receitas/receitas/enums/TipoReceita.java
+++ src/main/java/com/receitas/receitas/enums/TipoReceita.java
@@ -1,7 +1,15 @@
 package com.receitas.receitas.enums;
 
 public enum TipoReceita {
-    DOCES,
-    SALGADOS,
-    SOBREMESA
+    DOCE(1L, "DOCE"),
+    SALGADO(2L, "SALGADO");
+
+    private final Long id;
+    private final String name;
+
+    TipoReceita(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
 }
--- src/main/java/com/receitas/receitas/respository/ReceitaRepository.java
+++ src/main/java/com/receitas/receitas/respository/ReceitaRepository.java
@@ -2,6 +2,13 @@ package com.receitas.receitas.respository;
 
 import com.receitas.receitas.model.Receita;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
+@Repository
 public interface ReceitaRepository extends JpaRepository<Receita,Long> {
+
+    public Optional<Receita> findbyNome(String nome);
 }
